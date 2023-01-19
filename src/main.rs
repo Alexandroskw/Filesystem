517ca66c14470114dd4296337ed86bb40ca9c0ad
Alexandroskw <filipbilly.120@gmail.com>
2023-01-19 14:42:37 -0600

    Imprimo la etiqueta del volumen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::u8;
 use std::{io::prelude::*, fs::File, path::Path/*, error::Error*/};
 use std::process::Command;
 
@@ -21,39 +22,43 @@ fn sistema() {
 	unsafe { 
 		match file.read_to_end(&mut CLUSTERS) {
 			Err(e) => panic!("\n\tNo se ha podido leer {}: {}", path.display(), e.to_string()),
-			Ok(bytes) => println!("\n\tExito al leer los bytes {} de {}\n\n", bytes, path.display()),
+			Ok(bytes) => println!("\tExito al leer los bytes {} de {}\n\n", bytes, path.display()),
 		};
 		
 		let header: &[u8] = Default::default();
 
 		for (i, _) in CLUSTERS.iter().enumerate().step_by(BLOCK_SIZE) {
-			let header_name = cadenas(&CLUSTERS[0..(i+BLOCK_SIZE)].to_vec());
-			let header_ver = cadenas2(&CLUSTERS[10..(i+BLOCK_SIZE)].to_vec());
+			let _header_name = cadenas(&CLUSTERS[0..(i+BLOCK_SIZE)].to_vec());
+			let _header_ver: (String, String) = cadenas2(&CLUSTERS[10..(i+BLOCK_SIZE)].to_vec());
 		}
+
 		println!("\tNombre: {}", cadenas(&header));
-		println!("\tVersion: {}", cadenas2(&header));
+		println!("\tVersión, Etiqueta del vol: {:?}", cadenas2(&header));
 	}
 }
 
-// Función para transformar a ASCII
+// Funciones para transformar a ASCII
+//Para mostrar nombre
 fn cadenas(_data: &[u8]) -> String {
 	unsafe {
 		let n = String::from_utf8(CLUSTERS[0..8].to_vec()).expect("ERROR AL LEER EL SÚPER BLOQUE");
 		return n;
 	}
 }
-
-fn cadenas2(_data: &[u8]) -> String {
+//Para mostrar versión y etiqueta del volumen
+fn cadenas2(_data: &[u8]) -> (String, String) {
 	unsafe {
 		let v = String::from_utf8(CLUSTERS[10..14].to_vec()).expect("ERROR AL LEER EL SÚPER BLOQUE");
-		return v;
+		let e = String::from_utf8(CLUSTERS[20..35].to_vec()).expect("ERROR AL LEER EL SÚPER BLOQUE");
+
+		return (v, e);
 	}
 }
 
 fn main() {
 	// Solo para limpiar la terminal
 	Command::new("clear").status().unwrap();
-	println!("\tSistema de Archivos de la Facultad de Ingenieria.");
+	println!("\tSistema de Archivos de la Facultad de Ingeniería.");
 	println!("\n\tBienvenido\n");
 	sistema();
 }
