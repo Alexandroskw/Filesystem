ac3a8db3534a16b6e606b488e970290fb684b441
Alexandroskw <filipbilly.120@gmail.com>
2024-01-17 18:40:02 -0600

    Agrego comentrios complementarios

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::io::Read;
 use std::fs::File;
 use std::path::{Path, self};
 
+// Estructura para los elementos del superbloque
 #[derive(Debug)]
-struct Superblock{
+struct Superblock{          
     fs_name: String,
     ver: String,
     vol_label: String,
@@ -12,17 +13,30 @@ struct Superblock{
     total_cluster: u32,
 }
 
+// Estructura para listar todos los archivos dentro del FS
+#[derive(Debug)]
+struct dirEntry{
+    file_type: char,
+    name: String,
+    size: u32,
+    initial_cluster: u32,
+    create_date: String,
+    modif_date: String,
+    unused: String,
+}
+
+// Función para leer el superbloque
 fn read_superblock(path: &str) -> Result<Superblock, String>{
-    let mut file = File::open(path).map_err(|e| format!("No se pudo montar: {}", path))?;
-    let mut buffer = [0u8; 54];
+    let mut file = File::open(path).map_err(|e| format!("No se pudo montar: {}", path))?;           // Se da la ruta del sistema de archivos y si no existe, lanza el error
+    let mut buffer = [0u8; 54];         // Se crea el búfer del tamaño del superbloque
 
-    file.read_exact(&mut buffer).map_err(|e| format!("No se pudo montar: {}", path))?;
+    file.read_exact(&mut buffer).map_err(|e| format!("No se pudo montar: {}", path))?;              // Si no existe el superbloque lanza el aviso de que no se pudo montar
 
-    let fs_name = String::from_utf8_lossy(&buffer[0..8]).trim().to_string();
+    let fs_name = String::from_utf8_lossy(&buffer[0..8]).trim().to_string();                // Se leen las primeras nueve posiciones del arreglo del superbloque con el búfer y haciendo borrowing del mismo
     let ver = String::from_utf8_lossy(&buffer[10..14]).trim().to_string();
-    let vol_label = String::from_utf8_lossy(&buffer[20..35]).trim().to_string();
-    let cluster_size = u32::from_le_bytes(buffer[40..44].try_into().unwrap());
-    let dir_cluster = u32::from_le_bytes(buffer[45..49].try_into().unwrap());
+    let vol_label = String::from_utf8_lossy(&buffer[20..35]).trim().to_string();            
+    let cluster_size = u32::from_le_bytes(buffer[40..44].try_into().unwrap());                 // Para hacer uso del formato little-endian, se usa la función from_le_bytes para hacer la correcta interpretación de este formato
+    let dir_cluster = u32::from_le_bytes(buffer[45..49].try_into().unwrap());                  // try_into() es una especie de  "try-catch", con el adicional de que si no puede transformar el tipo de dato, manda un error
     let total_cluster = u32::from_le_bytes(buffer[50..54].try_into().unwrap());
 
     Ok(Superblock{
